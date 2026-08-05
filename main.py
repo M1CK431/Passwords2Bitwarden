737227e89be61fec3b1213e2ed9f888216032721
M1CK431 <31622263+M1CK431@users.noreply.github.com>
2026-08-06 01:48:34 +0200

    make main.py self-contained

--- main.py
+++ main.py
@@ -4,8 +4,27 @@ import zipfile
 import os
 import argparse
 import sys
+import re
 
-from utils import parse_custom_fields
+
+def parse_custom_fields(fields):
+    """Parse Bitwarden custom fields string into a list of dicts."""
+    custom_fields = []
+    pattern = re.compile(r"(\S+), (\w+): (.+)")
+    for field in pattern.findall(fields):
+        match field[1]:
+            case "text" | "email" | "file" | "website":
+                field_type = 0  # Text field
+            case "secret":
+                field_type = 1  # Hidden field
+            case _:
+                field_type = 0  # Defaults to text field
+        custom_fields.append({
+            "name": field[0],
+            "value": field[2],
+            "type": field_type,
+        })
+    return custom_fields
 
 
 def main():
